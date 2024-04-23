b3a0a6ab9799b49d47e6fe8b18b146a37645fc7d
Vivian B-P <violetbp@eecs.umich.edu>
2024-04-23 11:47:02 -0400

    their code

--- main.tf
+++ main.tf
@@ -14,21 +14,35 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
 
   tags = {
     Terraform = "true"
     Environment = "dev"
   }
+}
+
+
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
+
+  name = "blog"
+
+  min_size            = 1
+  max_size            = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
@@ -66,42 +80,13 @@ module "blog_alb" {
 }
 
 module "blog_sg" {
-  name = "blog_SG_new"
-
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = module.blog_vpc.vpc_id         #attach to default vpc
-
-  ingress_rules = [
-    "http-80-tcp",
-    "https-443-tcp"
-  ]
+  vpc_id  = module.blog_vpc.vpc_id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules = [
-    "all-all"
-  ]
+  egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
-
-
-}
-
-
-module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "7.4.1"
-
-  # Autoscaling group
-  name = "blog"  # replacing instance
-  min_size = 1
-  max_size = 2
-
-  vpc_zone_identifier = module.blog_vpc.public_subnets 
-  target_group_arns   = module.blog_alb.target_group_arns   #amazon resouce numbers
-  security_groups = [module.blog_sg.security_group_id]
-
-
-  image_id      = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
 }
