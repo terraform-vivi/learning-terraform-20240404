b9c661328b5a268fa238d4738629d097a2af4e17
Vivian B-P <violetbp@eecs.umich.edu>
2024-04-23 10:15:07 -0400

    create new dev vpc

--- main.tf
+++ main.tf
@@ -14,15 +14,29 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
+
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
   vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "HelloWorld"
@@ -35,7 +49,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = data.aws_vpc.default.id          #attach to default vpc
+  vpc_id  = module.blog_vpc.vpc_id         #attach to default vpc
 
   ingress_rules = [
     "http-80-tcp",
@@ -49,6 +63,32 @@ module "blog_sg" {
   egress_cidr_blocks = ["0.0.0.0/0"]
 
 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+data "aws_vpc" "default" {
+  default = true
 }
 
 
